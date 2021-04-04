d755aa881dbb614b8496a5150ce09eb728bdcfd5
Sandeep GC <sandeep_gc@hotmail.com>
2021-04-04 16:15:31 +1000

    edited the code for the time blocks

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var timeDisplayEl = $('#time-display');
 
+// handle displaying the time
 function displayTime() {
+  // Use Moment.js to format
   var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
 }
@@ -11,5 +13,6 @@ var saveButton = $('#button');
 saveButton.on('click', function (event) {
   // Prevent default action
   event.preventDefault();
-  
 });
+submitEl.addEventListener("click");
+
