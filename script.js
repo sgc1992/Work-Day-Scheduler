ef9dca5f614d56ae0769a63e26089b60b4b80ed0
Sandeep GC <sandeep_gc@hotmail.com>
2021-04-05 13:08:00 +1000

    added id for the time block

--- script.js
+++ script.js
@@ -8,14 +8,7 @@ function displayTime() {
 }
 setInterval(displayTime, 1000);
 
-var form = $('#form');
-var input = $('#text');
-var submit = $('#button');
 
-submit.onclick = function(event){
-  event.preventDefault()
-  var workDescription = text.value;
-}
 
 
 
