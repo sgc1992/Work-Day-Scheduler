7c69a4dd847bc3634980c7d706884b85a8e2364d
Sandeep GC <sandeep_gc@hotmail.com>
2021-04-05 18:08:42 +1000

    get item for the local storage

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ $(".saveBtn").on("click", function () {
   // Save text in local storage
   localStorage.setItem(time, text);
 })
+// Get item from local storage if any
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
